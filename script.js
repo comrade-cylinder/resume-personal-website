df12e7097bce688b5aa8c57bb6e1257232c07a18
comrade-cylinder <238068291+comrade-cylinder@users.noreply.github.com>
2025-12-17 11:02:39 +0100

    add icons add styling and JS to make nav menu work add seal easter egg (kinda broken but i'm taking a smoke break now )

---
+++ script.js
@@ -0,0 +1,40 @@
+const menuToggle = document.getElementById('menu-toggle');
+const navMenu = document.getElementById('nav-menu');
+menuToggle.addEventListener('click', () => {
+    const isOpen = navMenu.classList.toggle('open');
+    if (isOpen) {
+        setTimeout(() => {
+            document.addEventListener('click', handleOutsideClick);
+        }, 0);
+
+    }
+    else {
+        document.removeEventListener('click', handleOutsideClick);
+    }
+});
+
+const menuClose = document.getElementById('menu-close');
+
+menuClose.addEventListener('click', () => {
+    navMenu.classList.remove('open');
+    document.removeEventListener('click', handleOutsideClick)
+});
+
+//this function handles clicks outside of the menu 
+function handleOutsideClick (event) {
+    if (!event.target.closest('#nav-menu')) {
+        navMenu.classList.remove('open');
+        document.removeEventListener('click', handleOutsideClick); //evenetlistener cleanup
+    }
+}
+
+const sealLink = document.getElementById('seal-link');
+const gif = document.getElementById('seal-gif');
+
+sealLink.addEventListener('click', (e) => {
+    e.preventDefault();
+    gif.style.opacity= '1';
+    setTimeout(() => {
+        gif.style.opacity = '0';
+    }, 4000);
+})
